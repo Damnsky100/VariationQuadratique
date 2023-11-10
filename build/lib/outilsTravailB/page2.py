76a93fa4f580486cea463bac0571f409ae628b69
Sébastien Caron <71739253+Damnsky100@users.noreply.github.com>
2023-11-10 17:54:08 -0500

    a

---
+++ build/lib/outilsTravailB/page2.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import functions as f  # Ensure this module contains the necessary functions
+import os
+# Setup Streamlit widgets to accept user input
+st.title("3D Quadratic Variation Analysis")
+
+
+chemin_relatif = "data3D/"
+path = os.path.join(os.getcwd(), chemin_relatif)
+
+
+# Load and clean data
+@st.cache
+def load_data():
+    typeList = ["Naive", "Mean"]
+    dateList = ["1990", "2001", "2007", "2018"]
+    data_cleaned = {}
+    for type in typeList:
+        for date in dateList:
+            # Create a unique key for each combination of type and date
+            key = f"{type}_{date}"
+            # Load the corresponding CSV file
+            df = pd.read_csv(f"{path}{type}{date}.csv")
+            
+            # Check if "Unnamed: 0" is in the columns and convert it to datetime
+            if "Unnamed: 0" in df.columns:
+                df["Unnamed: 0"] = pd.to_datetime(df["Unnamed: 0"])
+                df = df.set_index("Unnamed: 0")
+            else:
+                # If "Unnamed: 0" is not in columns, you may want to set the first column as index
+                # and convert it to datetime if it contains date information
+                df[df.columns[0]] = pd.to_datetime(df[df.columns[0]])
+                df = df.set_index(df.columns[0])
+            data_cleaned[key] = df
+                
+    return data_cleaned
+
+data_cleaned = load_data()
+
+# User inputs for the type and year
+selected_type = st.selectbox("Select the type", ["Naive", "Mean"])
+selected_year = st.selectbox("Select a year", ["1990", "2001", "2007", "2018"])
+
+# Fetch the dataframe based on user selection
+selected_df_key = f"{selected_type}_{selected_year}"
+selected_df = data_cleaned[selected_df_key]
+
+# Convert the date to a proper format if necessary
+# Assuming the dataframe has a 'Date' column which needs to be converted to datetime
+# selected_df['Date'] = pd.to_datetime(selected_df['Date'])
+
+# Create meshgrid for 3D surface plot
+x, y = np.meshgrid(selected_df.columns, selected_df.index)
+z = selected_df.to_numpy()
+
+# Create the 3D surface plot
+fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
+
+# Update the layout of the figure
+fig.update_layout(
+    title=f"{selected_type} Quadratic Variation - {selected_year}",
+    scene=dict(
+        xaxis_title="n pas par jours",
+        yaxis_title="Date",
+        zaxis_title="Quadratic Variation"
+    ),
+    autosize=False,
+    width=800,
+    height=800,
+    margin=dict(l=65, r=50, b=65, t=90)
+)
+
+# Display the figure in Streamlit
+st.plotly_chart(fig)
---
+++ build/lib/outilsTravailB/page4.py
@@ -0,0 +1,86 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objs as go
+import os
+
+# Set your path
+
+chemin_relatif = "data2D/"
+path = os.path.join(os.getcwd(), chemin_relatif)
+
+@st.cache
+def load_data(year):
+    data = pd.read_csv(f"{path}data{year}.csv")
+    data['Date'] = pd.to_datetime(data['Date'])
+    # Set 'dailyDate' as the index
+    data.set_index('Date', inplace=True)
+    # Adding new columns for analysis
+    data['Open-Close (%)'] = ((data['Open'] - data['Close']) / data['Close']) * 100  # Convert to percentage
+    data['High-Low (%)'] = ((data['High'] - data['Low']) / data['Close']) * 100  # Convert to percentage
+    return data
+
+st.title("Data Analysis of Financial Contracts")
+
+# User selects the year
+selected_year = st.selectbox("Select a year", [1990, 2001, 2007, 2018])
+df = load_data(selected_year)
+
+# Display constant info
+contract_name = df['ContractName'].iloc[0]
+time_zone = df['TimeZone'].iloc[0]
+st.write(f"Contract Name: {contract_name}, Time Zone: {time_zone}")
+
+# High-Low Graph in Percentage
+st.subheader("High-Low Difference Over Time (Percentage)")
+fig_high_low = go.Figure(data=[go.Scatter(x=df.index, y=df['High-Low (%)'], mode='lines')])
+fig_high_low.update_layout(title='High-Low Difference Through Time (Percentage)', xaxis_title='Date', yaxis_title='High-Low Difference (%)')
+st.plotly_chart(fig_high_low)
+
+# OHLC Chart
+"""st.subheader("OHLC Chart")
+fig_ohlc = go.Figure(data=[go.Ohlc(x=df.index, open=df['Open'], high=df['High'], low=df['Low'], close=df['Close'])])
+fig_ohlc.update_layout(title='OHLC Data', xaxis_title='Date', yaxis_title='Price')
+st.plotly_chart(fig_ohlc)"""
+
+nbtrade = df.groupby("dailyDate")['NbTrade'].sum()
+# Number of Trades Graph
+st.subheader(f"Number of Daily Trades Over Time : {selected_year}")
+fig_nb_trade = go.Figure(data=[go.Scatter(x=nbtrade.index, y=nbtrade, mode='lines')])
+fig_nb_trade.update_layout(title='Number of Trades Over Time', xaxis_title='Date', yaxis_title='Number of Trades')
+st.plotly_chart(fig_nb_trade)
+# Columns to analyze
+columns_to_analyze = ['Open', 'Close', 'High', 'Low', 'LastTradedPrice', 
+                      'LagSameContractLastTradedPrice', 'Volume', 'NbTrade', 'LogReturn',
+                      'Open-Close (%)', 'High-Low (%)']
+
+# Show statistical metrics
+stats_data = {}
+for col in columns_to_analyze:
+    stats_data[col] = df[col].describe()
+
+# Create a big table for all the stats
+stats_df = pd.DataFrame(stats_data)
+st.subheader(f"Descriptive Statistics {selected_year} - Period : Minute")
+st.write(stats_df.transpose())
+
+# Visualizations for specific columns
+columns_for_distribution = ['NbTrade', 'LogReturn', 'Open-Close (%)', 'High-Low (%)']
+for col in columns_for_distribution:
+    st.subheader(f"Analysis of {col}")
+    fig = px.histogram(df, x=col, title=f"Distribution of {col}")
+    st.plotly_chart(fig)
+
+# Creating the Data Table
+start_date = df['dailyDate'].iloc[0]
+end_date = df['dailyDate'].iloc[-1]
+na_dates = df[df.isna().any(axis=1)]['dailyDate'].unique()
+
+st.subheader(f"Summary Table : {selected_year}")
+summary_table = pd.DataFrame({
+    "Start Date": [start_date],
+    "End Date": [end_date],
+    "Dates with NaN": [na_dates]
+})
+st.write(summary_table)
