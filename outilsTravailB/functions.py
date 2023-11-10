ee0932c83adb9e15f7166c278c6ddd2fc9f3ee92
Sébastien Caron <71739253+Damnsky100@users.noreply.github.com>
2023-11-10 18:11:06 -0500

    a

--- outilsTravailB/functions.py
+++ outilsTravailB/functions.py
@@ -73,4 +73,26 @@ def groupByDay(tmp : pd.DataFrame, type) :
 
 
 def runApp():
-    subprocess.run(["streamlit", "run", "app.py"])
+    import streamlit as st
+    import os
+    # Use Streamlit's command line option to run the multipage app
+    # streamlit run app.py
+    # app.py
+    # Define the pages
+    pages = {
+        "Quadratic Variation Analysis": "page1",
+        "3D Analysis": "page2",
+        "Quadratic Variation - Decomposition" : "page3",
+        "Data Analysis" : "page4"
+    }
+
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Go to", list(pages.keys()))
+
+
+
+    # Load the selected page
+    page = pages[selection]
+    with open(page + ".py") as f:
+        code = compile(f.read(), page + ".py", 'exec')
+        exec(code, {'__name__':'__main__'})
